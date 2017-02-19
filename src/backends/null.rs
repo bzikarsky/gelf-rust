ffbfddf5f3ea472daa692de78e1ba32799de9103
Benjamin Zikarsky <bzi@codemanufaktur.com>
2017-02-19 15:58:03 +0100

    Add docs to NullBackend

--- src/backends/null.rs
+++ src/backends/null.rs
@@ -2,9 +2,11 @@ use backends::Backend;
 use message::WireMessage;
 use errors::Result;
 
+/// The `NullBackend` is a utility backend which discards all messages
 pub struct NullBackend;
 
 impl NullBackend {
+    /// Construct a new NullBackend
     pub fn new() -> NullBackend {
         NullBackend {}
     }
@@ -15,6 +17,9 @@ impl Backend for NullBackend {
         false
     }
 
+    /// Log a message.
+    ///
+    /// Logging a message with NullBackend is a noop and will never fail.
     fn log_message(&self, _: WireMessage) -> Result<()> {
         Ok(())
     }
